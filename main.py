6f6e5d3d6763282ad712a304911e46b9950c3683
Yurii Nikolaenko <nikolaenko@gmail.com>
2022-09-04 13:12:09 +0300

    Add open file

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def delete_empty_folders(total_delete):
 def root_script():
     start_time = time.asctime()
 
-    for total_delete in folders:
+    for total_delete in folders_list:
         delete_old_files(total_delete)
         delete_empty_folders(total_delete)
 
@@ -40,8 +40,12 @@ def root_script():
 
 if __name__ == '__main__':
     time_new = 15  # Сколько дней оставить.
-    # folders = [r'D:\Arhive_All' r'\1C\BUH', r'D:\Arhive_All' r'\1C\OK']  # Папки в которых проверять файлы.
-    folders = [r'C:\Python_and_Django\Test_del_files\Alex', r'C:\Python_and_Django\Test_del_files\Yurii']  # Папки в которых проверять файлы.
+
+    open_folders = os.path.join('data_file', 'folders_file.txt')  # Путь и имя файла
+    with open (open_folders) as folders:  # Менеджеры контекста блокирования и разблокирования ресурсов, автоматического закрытия файлов.
+        folders_list = [line.rstrip() for line in folders
+                        if line != '/n' and not line.startswith('#')]  # Считать строки и исключить перенос строки и символа \
+        # rstrip() также удаляет любой пробел в конце строки и /n
 
     total_delete_file = 0  # Количество удалённых файлов.
     total_delete_folder = 0  # Количество удалённых папок.
@@ -49,4 +53,4 @@ if __name__ == '__main__':
     now_time = time.time()  # Определяем текущую дату.
     age_time = now_time - 60*60*24*time_new  # Вычисляем дату с которой надо удалять файлы.
 
-    # root_script()
+    root_script()
